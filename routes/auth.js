23f24c942e83e1408e9aad07513f596668dfe527
Akilan <testicleburnt@gmail.com>
2025-10-07 00:19:52 +0530

    Fix case-sensitive imports for Linux deployment

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const User = require('../models/user');
+const User = require('../models/User');
 const { generateToken } = require('../config/auth');
 const { protect } = require('../middleware/auth');
 
